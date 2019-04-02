aa7df28f7d53e8f2ee9602f8bf43bd1f0e37f532
MitchellGalvao <m.galvao@campus.fct.unl.pt>
2019-04-02 15:45:11 +0100

    Add files via upload

--- P4-Falhas/SD2019-Labs-P4/src/impl/clt/rest/DownloadMediaClient.java
+++ P4-Falhas/SD2019-Labs-P4/src/impl/clt/rest/DownloadMediaClient.java
@@ -1,20 +1,24 @@
 package impl.clt.rest;
 
 import java.io.IOException;
+import java.net.URI;
 
+import javax.ws.rs.ProcessingException;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
+import javax.ws.rs.core.UriBuilder;
 
 import org.glassfish.jersey.client.ClientConfig;
 import org.glassfish.jersey.client.ClientProperties;
 
 public class DownloadMediaClient {
-	private static final int READ_TIMEOUT = 2000;
-	private static final int CONNECT_TIMEOUT = 10000;
+	private static final int READ_TIMEOUT = 5000;
+	private static final int CONNECT_TIMEOUT = 2000;
+	final static int RETRY_PERIOD = 1000;
 
 	public static void main(String[] args) throws IOException {
 		
@@ -25,18 +29,30 @@ public class DownloadMediaClient {
 
 		Client client = ClientBuilder.newClient(config);
 
-		String earthURI = "http://localhost:9999/rest/media/84486F586FA514F31F07057F39B68C673B7A091F";
-		WebTarget target = client.target( earthURI );
+		URI baseURI = UriBuilder.fromUri("http://localhost:9999/rest/media").build();
+		WebTarget target = client.target( baseURI );
 		
-		Response r = target.request()
+		for(;;)
+		    try {
+		    	Response r = target.request()
 				.accept(MediaType.APPLICATION_OCTET_STREAM)
 				.get();
 
-		if( r.getStatus() == Status.OK.getStatusCode() && r.hasEntity() )
-			System.out.println("Response: " + (r.readEntity(byte[].class)).length);
-		else
-			System.out.println("Status: " + r.getStatus() );
+		    	if( r.getStatus() == Status.OK.getStatusCode() && r.hasEntity() )
+		    		System.out.println("Response: " + (r.readEntity(byte[].class)).length);
+		    	else
+		    		System.out.println("Status: " + r.getStatus() );
 
-	}
+		    	break;
+		    		} catch( ProcessingException pe ) {
+		    			try {
+		    				Thread.sleep( RETRY_PERIOD );
+		    			} catch (InterruptedException e) {
+		    				// TODO Auto-generated catch block
+		    				e.printStackTrace();
+		    			}
+		    		}
+
+	    }
 
 }
