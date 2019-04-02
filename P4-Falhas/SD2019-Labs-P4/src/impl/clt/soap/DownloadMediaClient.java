c04622552d00e70a1dda1fc91e8f98f5b7bb1758
MitchellGalvao <m.galvao@campus.fct.unl.pt>
2019-04-02 15:46:44 +0100

    Add files via upload

--- P4-Falhas/SD2019-Labs-P4/src/impl/clt/soap/DownloadMediaClient.java
+++ P4-Falhas/SD2019-Labs-P4/src/impl/clt/soap/DownloadMediaClient.java
@@ -6,6 +6,7 @@ import java.util.logging.Logger;
 
 import javax.xml.namespace.QName;
 import javax.xml.ws.Service;
+import javax.xml.ws.WebServiceException;
 
 import impl.srv.soap.SoapMediaStorageServer;
 import microgram.api.soap.MicrogramException;
@@ -19,24 +20,37 @@ public class DownloadMediaClient {
 	private static Logger Log = Logger.getLogger(DownloadMediaClient.class.getName());
 
 	private static final String WSDL = "?wsdl";
+	final static int RETRY_PERIOD = 1000;
 
 	public static void main(String[] args) throws IOException {
 
 		String serverUrl = args.length > 0 ? args[0]
 				: String.format("http://localhost:%s/soap/%s", SoapMediaStorageServer.PORT, SoapMedia.NAME);
 
+		
 		QName QNAME = new QName(SoapMedia.NAMESPACE, SoapMedia.NAME);
-		try {
-			Service service = Service.create(new URL(serverUrl + WSDL), QNAME);
-			SoapMedia media = service.getPort(microgram.api.soap.SoapMedia.class);
 
-			String id = "84486F586FA514F31F07057F39B68C673B7A091F";
-			byte[] bytes = media.download(id);
-			Log.info("Download completed: " + bytes.length + " bytes...");
+		for(;;)
+		    try {
+		        Service service = Service.create( new URL(serverUrl + WSDL), QNAME);
+		        SoapMedia media = service.getPort( microgram.api.soap.SoapMedia.class );
 
-		} catch (MicrogramException x) {
-			Log.info("Download failed, reason: " + x.getMessage());
-		} 
-	}
-
+		        String id = "84486F586FA514F31F07057F39B68C673B7A091F";
+				byte[] bytes = media.download(id);
+				Log.info("Download completed: " + bytes.length + " bytes...");;
+		        break;
+		    } catch( MicrogramException x ) {
+		    	Log.info("Download failed, reason: " + x.getMessage());
+		        break;
+		    } catch( WebServiceException ws ) {
+		        Log.info("IO error, reason: "  + ws.getMessage());
+		        try {
+					Thread.sleep( RETRY_PERIOD);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+		    }
+		}
+					
 }
